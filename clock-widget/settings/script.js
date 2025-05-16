8c8f134968fe86fa201983c59cacdac619b729d6
nuttylmao <nuttylmao@gmail.com>
2025-05-17 06:48:58 +1000

    Added a basic settings page to many widgets

---
+++ clock-widget/settings/script.js
@@ -0,0 +1,23 @@
+const widgetContainer = document.getElementById('widgetContainer');
+
+const settingsPageURL = '../../.utilities/settings-page-builder';
+
+const currentURL = window.location.href;
+
+let settingsJSON;
+let baseURL = currentURL;
+
+if (baseURL.endsWith("index.html"))
+    baseURL = baseURL.replace("index.html", "");
+
+settingsJSON = "?settingsJson=" + baseURL + "settings.json";
+
+const lastSlashIndex = baseURL.lastIndexOf("/");
+let widgetURL = "&widgetURL=" + baseURL.replace("/settings", "");
+
+console.debug("Window Ref: " + window.location.href);
+console.debug("Base URL: " + baseURL);
+console.debug("Settings JSON: " + settingsJSON);
+console.debug("Widget URL: " + widgetURL);
+
+widgetContainer.src = settingsPageURL + settingsJSON + widgetURL;
