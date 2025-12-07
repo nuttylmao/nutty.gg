b52b7eb7c4e746cf408120e30678820116631121
rexbordz <rexbordz@users.noreply.github.com>
2025-12-07 19:32:07 +0800

    Fixed the Random Hex function (hopefully)

--- chatrd/js/modules/tiktok/module.js
+++ chatrd/js/modules/tiktok/module.js
@@ -27,6 +27,8 @@ const tiktokGiftsClasses = [
     { min: 100000,  max: 99999999999, class: 'bigger-than-100000' },
 ];
 
+userColors.set('tiktok', new Map());
+
 document.addEventListener('DOMContentLoaded', () => {
     if (showTiktok) {
         const tiktokStatistics = `
