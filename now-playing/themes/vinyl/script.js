18ee927009b101c623d584b454e46ad604f29499
nutty <nuttylmao@gmail.com>
2026-06-30 04:38:15 +1000

    Added a 'Vinyl' theme

--- now-playing/themes/vinyl/script.js
+++ now-playing/themes/vinyl/script.js
@@ -4,17 +4,17 @@
 
 const mainWrapper = document.getElementById('main-wrapper');
 const albumArtContainer = document.getElementById('album-art-container');
-const songInfoContainer = document.getElementById('song-info-container');
+// const songInfoContainer = document.getElementById('song-info-container');
 const albumArtLayer = document.getElementById('album-art-layer');
 const albumArtTransition = document.getElementById('album-art-transition-layer');
-// const backgroundLayer = document.getElementById('background-layer');
-// const backgroundTransitionLayer = document.getElementById('background-transition-layer');
-const trackLabel = document.getElementById('track-label');
-const artistLabel = document.getElementById('artist-label');
-// const progressContainer = document.getElementById('progress-container');
-// const progressBarFill = document.getElementById('progress-bar-fill');
-// const currentTimeLabel = document.getElementById('current-time');
-// const durationLabel = document.getElementById('duration');
+const progressCircle = document.getElementById('progress-pie-circle');
+
+///////////////
+// CONSTANTS //
+///////////////
+
+const circumference = 157.1;
+
 
 
 
@@ -22,19 +22,15 @@ const artistLabel = document.getElementById('artist-label');
 // PAGE SETUP //
 ////////////////
 
-// Set property visibility
-trackLabel.style.display = showPrimary ? '' : 'none';
-artistLabel.style.display = showSecondary ? '' : 'none';
-
 // Set container width
 if (maxWidth > 0)
     mainWrapper.style.width = `${maxWidth}px`;
 else
     mainWrapper.style.width = `100%`;
 
-// // Set progress bar visibility
-// if (!showProgressBar)
-//     progressContainer.style.display = 'none';
+// Set progress bar visibility
+if (!showProgressBar)
+    progressCircle.style.display = 'none';
 
 
 
@@ -43,7 +39,7 @@ else
 ////////////////////
 
 // Each theme must implement the following
-function UpdatePlayerState(data) {
+async function UpdatePlayerState(data) {
     // Check if the user has provided a target application in the settings
     const isFiltering = targetApplication && targetApplication.trim() !== "";
 
@@ -64,6 +60,9 @@ function UpdatePlayerState(data) {
         const playbackInfo = targetSession.playback_info;
         const mediaProps = targetSession.media_properties;
         const timelineProps = targetSession.timeline_properties;
+        
+        // Calcualte an accent color
+        const accentColorPalette = await GetAccentPalette(mediaProps.Thumbnail);
 
         // 1. Check if playback status has changed and update visibility accordingly
         if (playbackInfo.PlaybackStatus !== CurrentPlaybackStatus) {
@@ -85,34 +84,32 @@ function UpdatePlayerState(data) {
             }
         }
 
-        // // 3. Update the progress info
-        // if (timelineProps)
-        // {
-        //     // Parse the Windows timestamp into a JavaScript time object
-        //     const lastUpdateAnchor = Date.parse(timelineProps.LastUpdatedTime.replace(' ', 'T'));
+        // 3. Update the progress info
+        if (timelineProps)
+        {
+            // Parse the Windows timestamp into a JavaScript time object
+            const lastUpdateAnchor = Date.parse(timelineProps.LastUpdatedTime.replace(' ', 'T'));
 
-        //     // Calculate the drift (i.e. how many milliseconds have passed since Windows last updated)
-        //     const driftMs = Date.now() - lastUpdateAnchor;
+            // Calculate the drift (i.e. how many milliseconds have passed since Windows last updated)
+            const driftMs = Date.now() - lastUpdateAnchor;
 
-        //     // Add that drift to the reported Position
-        //     // Only add drift if the status is PLAYING
-        //     const isPlaying = (targetSession.playback_info.PlaybackStatus === PlaybackStatus.PLAYING);
-        //     const currentPositionMs = isPlaying && timelineProps.EndTime > 0 ? timelineProps.Position + driftMs : timelineProps.Position;
+            // Add that drift to the reported Position
+            // Only add drift if the status is PLAYING
+            const isPlaying = (targetSession.playback_info.PlaybackStatus === PlaybackStatus.PLAYING);
+            const currentPositionMs = isPlaying && timelineProps.EndTime > 0 ? timelineProps.Position + driftMs : timelineProps.Position;
 
-        //     // Update the label using your naming convention
-        //     currentTimeLabel.innerText =
-        //         ConvertMillisecondsToMinutesSoThatItLooksBetterOnTheOverlay(currentPositionMs);
-
-        //     durationLabel.innerText =
-        //         ConvertMillisecondsToMinutesSoThatItLooksBetterOnTheOverlay(timelineProps.EndTime);
-
-        //     // Set progressbar
-        //     // Ensure we don't divide by zero or exceed 100%
-        //     const durationMs = timelineProps.EndTime;
-        //     let progressPercent = durationMs > 0 ? (currentPositionMs / durationMs) * 100 : 0;
-        //     progressPercent = Math.min(100, Math.max(0, progressPercent));
-        //     progressBarFill.style.width = `${progressPercent}%`;
-        // }
+            // Set progressbar
+            // Ensure we don't divide by zero or exceed 100%
+            const durationMs = timelineProps.EndTime;
+            progressPercent = durationMs > 0 ? (currentPositionMs / durationMs) * 100 : 0;
+            progressPercent = Math.min(100, Math.max(0, progressPercent));
+            document.documentElement.style.setProperty('--accent-color', `${accentColorPalette.LightVibrant}`);
+            
+            // Calculate the offset. 
+            // 0% progress = 157.1 offset. 100% progress = 0 offset.
+            const offset = circumference - (progressPercent / 100) * circumference;
+            progressCircle.style.strokeDashoffset = offset;
+        }
     }
     else
     {
@@ -148,51 +145,23 @@ function SetVisibility(visible) {
 }
 
 async function ChangeTrack(mediaProps) {
-    // Fade in the overlay (shows the new text)
-    trackLabel.style.opacity = "0";
-    artistLabel.style.opacity = "0";
-    // backgroundLayer.style.opacity = "0";
+    // // Fade in the overlay (shows the new text)
     albumArtLayer.style.opacity = "0";
     
     // Wait for fade (0.5s), then swap the real text and hide overlay
     setTimeout(() => {
-        trackLabel.innerText = swapArtistTrack ? mediaProps.Artist : mediaProps.Title;
-        artistLabel.innerText = swapArtistTrack ? mediaProps.Title : mediaProps.Artist; 
 
         // Extract the image source string (use fallback if Windows has no art)
         const newArtUrl = mediaProps.Thumbnail;
 
         // Set the image
-        // backgroundLayer.style.backgroundImage = `url('${newArtUrl}')`;
         albumArtLayer.style.backgroundImage = `url('${newArtUrl}')`;
-
-        // // Apply a tint: Use 30% opacity of the accent color + a dark overlay for contrast
-        // // 'rgba(0,0,0,0.6)' ensures the text stays readable
-        // backgroundLayer.style.backgroundColor = accent + "80"; // 80 is 50% opacity in hex
-
-        trackLabel.style.opacity = "";
-        artistLabel.style.opacity = "";
-        // backgroundLayer.style.opacity = "";
         albumArtLayer.style.opacity = "";
 
         setTimeout(() => {
-            // // Set the image
-            // backgroundTransitionLayer.style.backgroundImage = `url('${newArtUrl}')`;
+            // Set the image
             albumArtTransition.style.backgroundImage = `url('${newArtUrl}')`;
-
-            // // Apply a tint: Use 30% opacity of the accent color + a dark overlay for contrast
-            // // 'rgba(0,0,0,0.6)' ensures the text stays readable
-            // backgroundTransitionLayer.style.backgroundColor = accent + "80"; // 80 is 50% opacity in hex
-
             SetVisibility(true); // Show the overlay for a few seconds if autoHide is enabled
         }, 250);
     }, 250);
 }
-
-function SetAlbumArtSize() {
-    const height = songInfoContainer.offsetHeight;
-    albumArtContainer.style.height = `${1.5 * height}px`;
-    albumArtContainer.style.width = `${1.5 * height}px`;
-}
-
-SetAlbumArtSize();
